4fc93a15d1bf9cab639c6090ef866a2616f68f17
AbduganiSotiboldiyev <91188722+AbduganiSotiboldiyev@users.noreply.github.com>
2024-06-25 19:07:57 +0900

    handlebars's partials

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 import express from "express"
-import path ,{dirname} from "path"
-import { fileURLToPath } from "url"
-import { engine } from "express-handlebars"
+
+import { create } from "express-handlebars"
 
 const app = express()
-const __file = fileURLToPath(import.meta.url)
-const __dirname = dirname(__file)
 
-app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
+const hbs = create({defaultLayout:"main", extname:"hbs"})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
 app.set('views', './views')
+
 
 app.get("/", (req, res) =>{
     res.render("index")
-    // res.sendFile(path.join(__dirname, "templates" , "index.html"))
-    // res.send("Main page")
+
 })
 
 app.get("/about", (req, res) =>{
-
     res.render("about")
-
-    // res.sendFile(path.join(__dirname, "templates" , "about.html"))
 })
 
 const PORT = process.env.PORT || 5100
