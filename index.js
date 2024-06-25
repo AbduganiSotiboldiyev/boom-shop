2c207e9fd4824d2b9cece392dd1e84470948f7de
AbduganiSotiboldiyev <91188722+AbduganiSotiboldiyev@users.noreply.github.com>
2024-06-25 18:31:08 +0900

    express js get method

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 import express from "express"
+import path ,{dirname} from "path"
+import { fileURLToPath } from "url"
 
 const app = express()
+const __file = fileURLToPath(import.meta.url)
+const __dirname = dirname(__file)
+
+
+
+app.get("/", (req, res) =>{
+
+    res.sendFile(path.join(__dirname, "templates" , "index.html"))
+    // res.send("Main page")
+})
+
+app.get("/about", (req, res) =>{
+    res.sendFile(path.join(__dirname, "templates" , "about.html"))
+})
+
 const PORT = process.env.PORT || 5100
 app.listen(5100, () => console.log(`Server is working on PORT : ${PORT}`))
