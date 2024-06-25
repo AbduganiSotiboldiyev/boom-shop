1ba5664f7eb63cc584567b5c268f29d50ef88eec
AbduganiSotiboldiyev <91188722+AbduganiSotiboldiyev@users.noreply.github.com>
2024-06-25 18:50:19 +0900

    express-handlebars

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 import express from "express"
 import path ,{dirname} from "path"
 import { fileURLToPath } from "url"
+import { engine } from "express-handlebars"
 
 const app = express()
 const __file = fileURLToPath(import.meta.url)
 const __dirname = dirname(__file)
 
-
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
 
 app.get("/", (req, res) =>{
-
-    res.sendFile(path.join(__dirname, "templates" , "index.html"))
+    res.render("index")
+    // res.sendFile(path.join(__dirname, "templates" , "index.html"))
     // res.send("Main page")
 })
 
 app.get("/about", (req, res) =>{
-    res.sendFile(path.join(__dirname, "templates" , "about.html"))
+
+    res.render("about")
+
+    // res.sendFile(path.join(__dirname, "templates" , "about.html"))
 })
 
 const PORT = process.env.PORT || 5100
