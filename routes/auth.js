a6d60f0bf52de3df048c8aee1b6e9039d811cba7
AbduganiSotiboldiyev <91188722+AbduganiSotiboldiyev@users.noreply.github.com>
2024-06-26 17:37:28 +0900

    Dynamic title and options

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,17 @@ import {Router} from "express"
 const router = Router()
 
 router.get("/login", (req,res) => {
-    res.render("login")
+    res.render("login",{
+        title:"Abdu || Login",
+        isLogin : true
+    })
 })
 
 router.get("/register", (req,res) => {
-    res.render("register")
+    res.render("register", {
+        title:"Abdu || Register",
+        isRegister : true
+    })
 })
 
 export default router
--- routes/products.js
+++ routes/products.js
@@ -3,16 +3,24 @@ import {Router} from "express"
 const router = Router()
 
 router.get("/", (req, res) =>{
-    res.render("index")
+    res.render("index",{
+        title: "Abdu || APP"
+    })
 
 })
 
 router.get("/products", (req,res) => {
-    res.render("products")
+    res.render("products",{
+        title: "Abdu || Products",
+        isProduct : true
+    })
 })
 
 router.get("/add", (req,res) => {
-    res.render("add")
+    res.render("add",{
+        title : "Abdu  || Add",
+        isAdd : true
+    })
 })
 
 export default router
