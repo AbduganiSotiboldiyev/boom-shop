7b1a2453c392ae799844a93af6e5472c563ae7e3
AbduganiSotiboldiyev <91188722+AbduganiSotiboldiyev@users.noreply.github.com>
2024-06-27 17:48:43 +0900

    Adding MongoDb and using mongoose

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express"
 import { create } from "express-handlebars"
+import mongoose from "mongoose"
+import * as dotenv from "dotenv"
+// routes
 import AuhtRouter from "./routes/auth.js"
 import ProductsRouter from "./routes/products.js"
 
-
+dotenv.config()
 const app = express()
 
 const hbs = create({defaultLayout:"main", extname:"hbs"})
@@ -13,12 +16,27 @@ app.set('view engine', 'hbs')
 app.set('views', './views')
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
-
+app.use(express.json())
 app.use(AuhtRouter)
 app.use(ProductsRouter)
 
 
+const startApp = () => {
+    try {
+        const options = {
+          socketTimeoutMS: 5000 // Set a 5-second timeout
+        };
+        
+        mongoose.connect(process.env.MONGO_URI, options)
+          .then(() => console.log('MongoDB connected successfully'))
+          .catch(err => console.error('Error connecting to MongoDB:', err));
+        
+        const PORT = process.env.PORT || 5100
+        app.listen(5100, () => console.log(`Server is working on PORT : ${PORT}`))
+        
+    } catch (error) {
+        console.log(error.message)
+    }
+}
 
-
-const PORT = process.env.PORT || 5100
-app.listen(5100, () => console.log(`Server is working on PORT : ${PORT}`))
+startApp()
